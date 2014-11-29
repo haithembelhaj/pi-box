2ef2ce2f49187d61705177a6b768522d2f271678
Nicolas Traeder <nt@dietaikonauten.com>
2014-11-29 18:13:32 +0100

    Added server logic for vlc piping

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var omx = require('./omx');
+var stream = require('stream');
+var fs = require('fs');
+var formidable = require('formidable');
+var spawn = require('child_process').spawn;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-omx.start({
-    input: 'test.mp4'
+//omx.start({
+//    input: 'test.mp4'
+//});
+
+var vlc    = spawn('/Applications/VLC.app/Contents/MacOS/VLC', ['-']);
+
+vlc.stdout.on('data', function (data) {
+    console.log('' + data);
+});
+
+vlc.stderr.on('data', function (data) {
+    console.log('vlc stderr: ' + data);
+});
+
+vlc.on('close', function (code) {
+    console.log('vlc process exited with code ' + code);
 });
 
 app.post('/control', function (req, res) {
@@ -18,6 +36,56 @@ app.post('/control', function (req, res) {
     omx[req.body.command]().then(function(){
         res.send('Hello World!')
     });
+});
+
+
+var writeToVLCStdin = function(data) {
+    vlc.stdin.write(data);
+};
+
+app.post('/upload', function (req, res) {
+
+    //vlc -
+
+
+
+    var form = new formidable.IncomingForm();
+    var writeStream = fs.createWriteStream('./test');
+    //var Writable = require('stream').Writable;
+
+    form.onPart = function (part) {
+        console.log('Log parts');
+        //console.log(part);
+
+        part.removeListener('data', writeToVLCStdin);
+
+        part.addListener('data', writeToVLCStdin);
+
+        part.addListener('end', function () {
+            res.send('love');
+
+        })
+
+    };
+
+    form.parse(req, function(err, fields, files) {
+        console.log(files);
+    });
+
+    //form.on('end', function() {
+    //
+    //    res.send('love');
+    //
+    //});
+
+
+
+
+    //console.info(req.body);
+
+    //omx[req.body.command]().then(function(){
+    //    res.send('Hello World!')
+    //});
 });
 
 var server = app.listen(3000, function () {
